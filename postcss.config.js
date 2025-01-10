757be60c00e6e317f8d274655e3b9e4011bc60ea
Stiffpixels <saifimuzammil2@gmail.com>
2025-01-10 13:08:53 +0530

    revamped the hero section

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,16 @@
 let tailwindConfig = process.env.HUGO_FILE_TAILWIND_CONFIG_JS || './tailwind.config.js';
 const tailwind = require('tailwindcss')(tailwindConfig);
+const postcssEnvFunction = require("postcss-env-function");
 const autoprefixer = require('autoprefixer');
 
 module.exports = {
-	// eslint-disable-next-line no-process-env
-	plugins: [tailwind, ...(process.env.HUGO_ENVIRONMENT === 'production' ? [autoprefixer] : [])],
+    // eslint-disable-next-line no-process-env
+    plugins: [tailwind, postcssEnvFunction({
+        environmentVariables: {
+            '--sm': '640px',
+            '--md': '768px',
+            '--lg': '1024px',
+            '--xl': '1536px'
+        },
+    }), ...(process.env.HUGO_ENVIRONMENT === 'production' ? [autoprefixer] : [])],
 };
